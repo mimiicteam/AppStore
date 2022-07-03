e0de1f7b4bb400bd5a167293f9dd0478892a300d
MINH DUC NGUYEN <mimiicteam@gmail.com>
2022-07-03 17:20:41 +0700

    Card View

--- AppStore/View/Home.swift
+++ AppStore/View/Home.swift
@@ -9,12 +9,129 @@ import SwiftUI
 
 struct Home: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(spacing : 30) {
+                HStack(alignment: .bottom, spacing: 30) {
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("SUNDAY, JULY 3")
+                            .font(.callout)
+                            .foregroundColor(.gray)
+                        
+                        Text("Today")
+                            .font(.largeTitle.bold())
+                    }
+                    
+                    Spacer()
+                    
+                    Button {
+                        
+                        
+                    } label: {
+                        Image("Profile")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 50, height: 50)
+                            .clipShape(Circle())
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal)
+                
+                ForEach(todayItem) { item in
+                    Button {
+                        
+                    } label: {
+                        CardView(item: item)
+                    }
+                }
+            }
+            .padding(.vertical)
+        }
+    }
+    
+    //MARK: - Card View
+    func CardView(item: Today) -> some View {
+        VStack(alignment: .leading, spacing: 15) {
+            ZStack(alignment: .topLeading) {
+                GeometryReader { proxy in
+                    let size = proxy.size
+                    Image(item.artwork)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: size.width, height: size.height)
+                        .clipShape(CustomCorner(corners: [.topLeft, .topRight], radius: 20))
+                }
+                .frame(height: 400)
+                
+                LinearGradient(colors: [
+                    .black.opacity(0.5),
+                    .black.opacity(0.2),
+                    .clear
+                ], startPoint: .top, endPoint: .bottom)
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text(item.platformTitle.uppercased())
+                        .foregroundColor(.gray)
+                        .font(.callout)
+                        .fontWeight(.semibold)
+                    
+                    Text(item.bannerTitle)
+                        .font(.largeTitle.bold())
+                        .foregroundColor(.primary)
+                }
+                .multilineTextAlignment(.leading)
+                .padding()
+            }
+            
+            HStack(spacing: 12) {
+                Image(item.appLogo)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 60, height: 60)
+                    .clipShape(RoundedRectangle(cornerRadius: 15, style: .continuous))
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(item.platformTitle.uppercased())
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                    
+                    Text(item.appName)
+                        .fontWeight(.bold)
+                    
+                    Text(item.appDescription)
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                }
+                .foregroundColor(.primary)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Button {
+                    
+                } label: {
+                    Text("GET")
+                        .fontWeight(.bold)
+                        .foregroundColor(.blue)
+                        .padding(.vertical, 8)
+                        .padding(.horizontal, 20)
+                        .background {
+                            Capsule()
+                                .fill(.ultraThinMaterial)
+                        }
+                        
+                }
+            }
+            .padding([.horizontal, .bottom])
+        }
+        .background {
+            RoundedRectangle(cornerRadius: 15, style: .continuous)
+                .fill(.gray.opacity(0.2))
+        }
     }
 }
 
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
