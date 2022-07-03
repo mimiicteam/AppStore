53af798b2547d414c645e815fb49e1dd516b1562
MINH DUC NGUYEN <mimiicteam@gmail.com>
2022-07-03 14:02:38 +0700

    Custom Corner

---
+++ AppStore/View/CustomCorner.swift
@@ -0,0 +1,19 @@
+//
+//  CustomCorner.swift
+//  AppStore
+//
+//  Created by MINH DUC NGUYEN on 03/07/2022.
+//
+
+import SwiftUI
+
+struct CustomCorner: Shape {
+    var corners: UIRectCorner
+    var radius: CGFloat
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
